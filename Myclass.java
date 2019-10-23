c206059e924b71132d5abf3256e944880410d28f
Asiri Senith Manjitha <asirisenith@gmail.com>
2019-10-23 23:29:22 +0530

    Define a for loop

---
+++ Myclass.java
@@ -0,0 +1,9 @@
+
+public class Myclass{
+    public static void main(String[] args){
+      int[] number = {1,2,3,4,5,6,7,8,9};
+      for(int i:number){
+          System.out.println(i);
+      }
+    }
+}
