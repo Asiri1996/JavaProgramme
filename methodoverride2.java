1b6906fdd42f32e607a07f322316f6d2ff00dac8
Asiri Senith Manjitha <asirisenith@gmail.com>
2019-11-15 22:13:28 +0530

    method override example 2 using super keyword

---
+++ methodoverride2.java
@@ -0,0 +1,17 @@
+class Animal{
+    public void displayInfo() {
+        System.out.println("I am an Animal.");
+    }
+}
+class Dog extends Animal{
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("I am a Dog.");
+    }
+}
+class methodoverride2{
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.displayInfo();
+    }
+}
