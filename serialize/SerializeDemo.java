7940e79bd9f52e1a951e267b90be11e6be70ec5c
Asiri Senith <asirisenith@gmail.com>
2020-08-09 01:24:46 +0530

    Created SerializeDemo.java file in serialize package

--- serialize/SerializeDemo.java
+++ serialize/SerializeDemo.java
@@ -1,11 +1,26 @@
 package serialize;
 
 import java.io.*;
-
+//import ".Employee.java";
 class SerializeDemo{
 
-    public static void main(String[] args) {
-        
+    public static void main(final String[] args) {
+       final Employee emp = new Employee();
+       emp.setName("Asiri");
+       emp.setAddress("Ananda vidyala mavatha, Elpitiya");
+       emp.setNIC(960283481);
+       emp.setNumber(101);
+       
+       try {
+           final FileOutputStream fileout = new FileOutputStream("employee.ser");
+           final ObjectOutputStream Objout = new ObjectOutputStream(fileout);
+           Objout.writeObject(emp);
+           Objout.close();
+           fileout.close();
+           System.out.println("Serialized data is saved in employee.ser");
+       } catch (final IOException i) {
+           i.printStackTrace();
+       }
     }
     
 }
