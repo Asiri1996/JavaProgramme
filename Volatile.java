1ef752ce9cc699728409ffdf5827b0302b93213f
Asiri Manjitha <56013619+AsiriZenith@users.noreply.github.com>
2021-06-05 15:16:16 +0530

    Volatile keyword in java

---
+++ Volatile.java
@@ -0,0 +1,63 @@
+/**
+ * VolatileData
+ */
+class VolatileData {
+    private volatile int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increaseCounter() {
+        ++counter;
+    }
+}
+
+/**
+ * VolatileThread
+ */
+class VolatileThread extends Thread{
+
+    private final VolatileData data;
+
+    public VolatileThread(VolatileData data) {
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        int oldValue = data.getCounter();
+        System.out.println("[Thread " + Thread.currentThread().getId() + "]: Old value = " + oldValue);
+        data.increaseCounter();
+        int newValue = data.getCounter();
+        System.out.println("[Thread " + Thread.currentThread().getId() + "]: New value = " + newValue);
+    }
+}
+
+/**
+ * Volatile
+ */
+public class Volatile {
+    
+    private final static int noOfThreads = 2;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        VolatileData volatileData = new VolatileData();
+
+        Thread[] threads = new Thread[noOfThreads];
+
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i] = new VolatileThread(volatileData);
+        }
+
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i].start();
+        }
+
+        for (int i = 0; i < noOfThreads; ++i) {
+            threads[i].join();
+        }
+        
+    }
+}
