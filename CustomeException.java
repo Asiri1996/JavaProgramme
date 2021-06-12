f15b593d1a959fdef8695a7dbe37dbc03e0c0e2a
Asiri Manjitha <56013619+AsiriZenith@users.noreply.github.com>
2021-06-12 11:12:01 +0530

    CustomeException example created

---
+++ CustomeException.java
@@ -0,0 +1,30 @@
+/**
+ * InvalidAgeException
+ */
+class InvalidAgeException extends Exception {
+
+    InvalidAgeException(String s){
+        super(s);
+    }
+}
+
+
+public class CustomeException{
+
+    public static void validate(int age) throws InvalidAgeException {
+        if (age<18) {
+            throw new InvalidAgeException("not valid");
+        } else {
+            System.out.println("welcome to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            validate(13);
+        } catch (Exception e) {
+            System.out.println("Exception occured : " + e);
+        }
+        System.out.println("rest of the code...");
+    }
+}
